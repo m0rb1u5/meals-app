9dc1f9beee154ee64175f300cccd80128f8236a8
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-06-12 12:49:20 -0300

    173. Replacing Screens (Instead of Pushing)

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/tabs.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -10,12 +11,27 @@ class FiltersScreen extends StatefulWidget {
 class _FiltersScreenState extends State<FiltersScreen> {
   bool _glutenFreeFilterSet = false;
 
+  // ignore: unused_element
+  void _setScreen(String identifier) {
+    Navigator.of(context).pop();
+    if (identifier == 'meals') {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (ctx) => const TabsScreen(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Filters'),
       ),
+      // drawer: MainDrawer(
+      //   onSelectScreen: _setScreen,
+      // ),
       body: Column(
         children: [
           SwitchListTile(
