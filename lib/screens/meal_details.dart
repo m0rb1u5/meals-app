480e245f87987ed66ae9209b7ead8334da8b02c3
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-06-14 11:27:45 -0300

    194. Swapping the 'Favorite Button' Based On Provider State

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -13,6 +13,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final List<Meal> favoriteMeals = ref.watch(favoriteMealsProvider);
+    final bool isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -31,7 +34,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           )
         ],
       ),
