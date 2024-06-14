82a3a80cbce17955dd4f7d8dff4be7637bdaa7b1
Juan Carlos Carbajal Ipenza <goesrex@gmail.com>
2024-06-14 11:45:23 -0300

    199. Explicit Animations: Adding an Animation Controller

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,7 +5,7 @@ import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availabelMeals,
@@ -13,8 +13,32 @@ class CategoriesScreen extends StatelessWidget {
 
   final List<Meal> availabelMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final List<Meal> filteredMeals = availabelMeals
+    final List<Meal> filteredMeals = widget.availabelMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
